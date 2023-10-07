caea0cde3f644d4025e5332d86a56e00a8317658
timriffe <tim.riffe@gmail.com>
2023-10-07 17:17:35 +0200

    change from calc_lt(0 to lt_flexible(), change its interior flow logic. Cut out the figure and make it into a figure function to be called separately, I guess after lt_flexible has run.

--- test.R
+++ test.R
@@ -316,61 +316,48 @@ ggplot() +
 
 #### HEre it is
 
-calc_lt <- function(Deaths    = abridged_data$Deaths, # replace with NULL. this is for demonstration purposes
-                    Exposures = abridged_data$Exposures,
-                    Age       = abridged_data$Age,
-                    OAnew     = 100,
-                    sing_year = TRUE, # CHECK! This is for option number 2 
-                    etrapFrom = 80,
-                    extrapFit = seq(60, 100, by = 5), # maybe somehow modify the argument? Not sure if needed
-                    radix     = 1e+05,
-                    extrapLaw = NULL,
-                    SRB       = 1.05,
-                    a0rule    = "ak",
-                    axmethod  = "un",
-                    Sex       = "m") {
-  # Idea: we can detect whether incoming ages are abridged or single. 
-  #  The user should just need to select whether they want abridged or single outgoing ages. Make sense?
-  # So out wrapper function would be calc_lt(), passing in all arguments.
-  # we have lt_single2abridged() and lt_abridged2single(), for instance for
-  extrapLaws <-
-    c("Kannisto",
-      "Kannisto_Makeham",
-      "Makeham",
-      "Gompertz",
-      "GGompertz",
-      "Beard",
-      "Beard_Makeham",
-      "Quadratic",
-      "UN_South_Asian",
-      "user")
+#' @param age_out character, either `"single"`, or `"abridged"`
+lt_flexible <- function(Deaths    = Deaths, # replace with NULL. this is for demonstration purposes
+                        Exposures = Exposures,
+                        Age       = Age,
+                        
+                        # recall all of these are passed in from the app, which will contain
+                        # its own default values.
+                        OAnew     = 100,
+                        age_out = "single", # CHECK! This is for option number 2 
+                        etrapFrom = 80,
+                        extrapFit = Age[Age >= 60], # maybe somehow modify the argument? Not sure if needed
+                        radix     = 1e+05,
+                        extrapLaw = NULL,
+                        SRB       = 1.05,
+                        a0rule    = "ak",
+                        axmethod  = "un",
+                        Sex       = "m") {
+  # TR: no need to determine extrapLaw here, it happens
+  # natively in the lt functions.
+
+
   
-  # which law to consider. QUESTION: Do we need this part?
-  # I did not write this mathing for other arguments since they are very simple and basically binary A or B
-  # this one requires some typing so mistakes might occur
-  if (is.null(extrapLaw) & max(Age) > 89) {
-    
-    extrapLaw <- "Kannisto"
-    
-  } else if (is.null(extrapLaw) & max(Age) < 90) {
-    
-    extrapLaw <- "makeham"
-    
-  } else { 
-    
-    extrapLaw <- match.arg(extrapLaw, extrapLaws, several.ok = FALSE) # was several.ok = FALSE
-    
+  age_in <- case_when(is_single(Age) ~ "single",
+                      is_abridged(Age) ~ "abridged",
+                      TRUE ~ "problem")
+  
+  # TR: this can become the checker function I guess
+  if (age_in == "problem"){
+    stop(
+      "Age groups appear irregular. Only single or standard abrdiged ages are supported now"
+    )
   }
   
-  Mx_emp <- Deaths/ Exposures
-  
-  # Option one. We can detect return the data similar to the input. LIne if single return single 
-  # Nice but probably not what we want
-  if(is_single(Age)) {
-    
-    data1 <- lt_abridged(Deaths     = abridged_data$Deaths,
-                         Exposures  = abridged_data$Exposures,
-                         Age        = abridged_data$Age,
+  # age_in and out_out both abridged
+  if(age_in == "abridged" & age_out == "abridged") {
+    # TR possibly more args to pass, or different arg management;
+    # for instance, construct a completed list of args
+    # and execute the function using do.call()
+    data_out <- lt_abridged(Deaths  = Deaths,
+                         Exposures  = Exposures,
+                         Age        = Age,
+                         AgeInt     = AgeInt,
                          OAnew      = OAnew,  
                          extrapFrom = extrapFrom,
                          extrapFit  = extrapFit,
@@ -380,58 +367,69 @@ calc_lt <- function(Deaths    = abridged_data$Deaths, # replace with NULL. this 
                          a0rule     = a0rule,
                          axmethod   = axmethod,
                          Sex        = Sex)
-    
-  } else { 
-    
-    data1 <- lt_abridged2single(Deaths     = abridged_data$Deaths,
-                                Exposures  = abridged_data$Exposures,
-                                Age        = abridged_data$Age,
-                                OAnew      = OAnew,
-                                extrapFrom = extrapFrom,
-                                extrapFit  = extrapFit, # should we change it here too to 1 year intervals?
-                                radix      = radix,
-                                extrapLaw  = extrapLaw,
-                                SRB        = SRB,
-                                a0rule     = a0rule,
-                                axmethod   = axmethod,
-                                Sex        = Sex)
-    
   }
   
-  
-  # option 2. We can directly ask user what he wants. Easy and I think this is what we want
-  if(sing_year) {
-    
-    data1 <- lt_abridged2single(Deaths     = abridged_data$Deaths,
-                                Exposures  = abridged_data$Exposures,
-                                Age        = abridged_data$Age,
-                                OAnew      = OAnew,
-                                extrapFrom = extrapFrom,
-                                extrapFit  = extrapFit, # should we change it here too to 1 year intervals?
-                                radix      = radix,
-                                extrapLaw  = extrapLaw,
-                                SRB        = SRB,
-                                a0rule     = a0rule,
-                                axmethod   = axmethod,
-                                Sex        = Sex)
-    
-  } else { 
-    
-    data1 <- lt_abridged(Deaths     = abridged_data$Deaths,
-                         Exposures  = abridged_data$Exposures,
-                         Age        = abridged_data$Age,
-                         OAnew      = OAnew,  
-                         extrapFrom = extrapFrom,
-                         extrapFit  = extrapFit,
-                         radix      = radix,
-                         extrapLaw  = extrapLaw,
-                         SRB        = SRB,
-                         a0rule     = a0rule,
-                         axmethod   = axmethod,
-                         Sex        = Sex)
-    
+  # age_in abridged and age_out single
+  if (age_in == "abridged" & age_out == "single"){
+    data_out <- lt_abridged2single(Deaths     = Deaths,
+                                   Exposures  = Exposures,
+                                   Age        = Age,
+                                   AgeInt     = AgeInt,
+                                   OAnew      = OAnew,  
+                                   extrapFrom = extrapFrom,
+                                   extrapFit  = extrapFit,
+                                   radix      = radix,
+                                   extrapLaw  = extrapLaw,
+                                   SRB        = SRB,
+                                   a0rule     = a0rule,
+                                   axmethod   = axmethod,
+                                   Sex        = Sex)
   }
   
+  # age_in single, calculate no matter whether we 
+  # keep single ages or want abridged output; if single
+  # data_out doesn't change again
+  if (age_in == "single"){
+    # useful in case we use lt_single_mx()
+    Mx_emp <- Deaths/ Exposures
+    # Don't check age_out yet here, because the abridge function requires a 
+    # precalculated lifetable, see below
+    # TR same story; arg management should be complete and strategic
+    data_out <- lt_single_mx(nMx        = Mx_emp,
+                             Age        = Age,
+                             OAnew      = OAnew,
+                             extrapFrom = extrapFrom,
+                             extrapFit  = extrapFit, # should we change it here too to 1 year intervals?
+                             radix      = radix,
+                             extrapLaw  = extrapLaw,
+                             SRB        = SRB,
+                             a0rule     = a0rule,
+                             axmethod   = axmethod,
+                             Sex        = Sex)
+    
+  }
+
+  # final case, age_in single and age_out abridged,
+  # requires the precalculated single-age lifetable
+  if (age_in == "single" & age_out == "abridged"){
+
+      data_out <- lt_single2abridged(lx  = data_out$lx, 
+                                     nLx = data_out$nLx, 
+                                     ex  = data_out$ex, 
+                                     Age = data_out$Age)
+      
+  }
+    
+  # now all cases handled
+  return(data_out)
+  
+}
+
+
+# This can be more modular, as in a separate call, please adjust so that it can just take these two
+# data objects? Or whatever you think makes sense. This won't currently run because I changed the args
+# and scoping, but just so you see this can also be modular
+make_figure <- function(data_out, data_in){
   # plot the results
   figure <- ggplot() + 
     geom_line(aes(x = Age, y = Mx_emp), linewidth = 0.8) + 
@@ -448,12 +446,12 @@ calc_lt <- function(Deaths    = abridged_data$Deaths, # replace with NULL. this 
   
   
   # return a list with both data and figure
-  return(lst(data   = data1,
+  return(lst(data   = out,
              figure = figure))
   
 }
 
 # check. Works
-calc_lt()
+lt_flexible()
 
 
