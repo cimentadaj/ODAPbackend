ee5ab2a860f92566d7b9d41e6ae92652864c1109
Rustam <rustam.tursunzade@gmail.com>
2023-10-11 16:15:50 +0200

    Here I have updated the corresponding function with the following functionality: readers: now relies either on partial name mathcing or position matching for columns and renames them according to the convenience. It also now uses the tsv format. NB we can I think automatically detect the delimiter in the file and read ANY delimited file into R. I have a commented code at the end of the script for example of tis. Nut sure if needed. Plots: Updated to work with the data_in, data_out. I have added one additional argument extrapFrom to show from where to bulit the extrapolated modelled data. Checkers: AgeInt removed from check_missing_cols. check_nas now allows for NA in OAG in AgeInt Now we check if age starts with 0 too. lifetable: no majour updates ALso the roxygen style documentation was created for every function (but we need to update the examples probably).

--- R/plots.R
+++ R/plots.R
@@ -1,11 +1,32 @@
 # This can be more modular, as in a separate call, please adjust so that it can just take these two
 # data objects? Or whatever you think makes sense. This won't currently run because I changed the args
 # and scoping, but just so you see this can also be modular
-make_figure <- function(data_out, data_in){
+
+
+#' Plots a line graph of the log10 transformed empirical mortality rate (Mx) data and a fitted data from a chosen age.
+#' @description 
+#' @param data_in tibble. Empirical numeric Mx value to be plotted and and numeric Age values to be plotted.
+#' @param data_out tibble. Modelled numeric Mx value to be plotted and and numeric Age values to be plotted.
+#' @param extrapFrom numeric. extrapFrom an age interval from which the lifetable was extrapolated.
+#' @return A line graph with black line corresponding to empirical Mx data and red line corresponding to modelled Mx data from the  chosen extrapFrom value. 
+#' @importFrom ggplot ggplot geom_line scale_x_continuous scale_y_log10 theme_light geom_vline labs theme element_text
+#' @importFrom scales label_log pretty_breaks
+#' @importFrom dplyr filter
+#' @examples1
+#' \dontrun{
+#' make_figure(data_in = data_in, data_out = data_out, extrapFrom = 60)
+#' }
+make_figure <- function(
+                        data_in, # raw mx to plot
+                        data_out, # the data from lt
+                        extrapFrom) {
   # plot the results
+  
+  Mx_emp <- data_in$Deaths/ data_in$Exposures
+  
   figure <- ggplot() + 
-    geom_line(aes(x = Age, y = Mx_emp), linewidth = 0.8) + 
-    geom_line(data = filter(data1, Age >= extrapFrom), aes(x = Age, y = nMx), lty = 2, col = "red", linewidth = 1) +
+    geom_line(aes(x = data_in$Age, y = Mx_emp), linewidth = 0.8) + 
+    geom_line(data = filter(data_out, Age >= extrapFrom), aes(x = Age, y = nMx), lty = 2, col = "red", linewidth = 1) +
     scale_x_continuous(breaks = pretty_breaks()) +
     scale_y_log10(labels = label_log(digits = 2)) +
     theme_light() +
@@ -18,8 +39,10 @@ make_figure <- function(data_out, data_in){
   
   
   # return a list with both data and figure
-  return(lst(data   = out,
-             figure = figure))
+  return(figure)
   
 }
 
+
+
+make_figure(data_in = abridged_data, data_out = data_out, extrapFrom = 60)
