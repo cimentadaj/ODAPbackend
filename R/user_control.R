dde5582f45d01c3675143255f584521fb167be1c
timriffe <tim.riffe@gmail.com>
2023-12-21 19:55:26 +0100

    um, this is maybe 90% of cases? But having a hard time anticipating where the hole is

--- R/user_control.R
+++ R/user_control.R
@@ -6,17 +6,28 @@
 #' @param rough_method the `method` argument of `smooth_age_5()`
 #' @param constrain_infants logical, if age 0 is a separate age class, shall we constrain its proportion within the age group 0-5 in the output? Default `TRUE`.
 #' @importFrom dplyr case_when
+#' @examples
+# data(pop1m_ind, package = "DemoTools")
+# data_in <- data.frame(Exposures = pop1m_ind,
+#                       Age = 0:100)
+# variable = "Exposures"
+# age_out = "single"
+# fine_method = "sprague"
+# rough_method = "KKN"
+# constrain_infants = TRUE
 smooth_flexible <- function(data_in,
                             variable = "Deaths",
                             age_out = c("single","abridged","5-year"),
                             fine_method = c("auto","none","sprague", 
                                             "beers(ord)", "beers(mod)", 
                                             "grabill", "pclm", "mono", 
-                                            "MAV", "uniform"),
+                                            "uniform"),
                             rough_method = c("auto","none","Carrier-Farrag", 
                                              "KKN", "Arriaga", 
                                               "United Nations", "Strong", 
-                                             "Zigzag", "MAV","pclm"),
+                                             "Zigzag", "pclm"),
+                            u5m = NULL,
+                            Sex = c("t","f","m"),
                             constrain_infants = TRUE){
   
   # ensure just one of each method is chosen. 
@@ -25,28 +36,59 @@ smooth_flexible <- function(data_in,
   # Likewise pclm rough is compatible with pclm fine; 
   # no pclm offsets in this implementation, and no explicit tail control.
   
-  rough_method <- match.arg(rough_method, c("auto","none","Carrier-Farrag", "KKN", "Arriaga", 
-                                "United Nations", "Strong", "Zigzag", "MAV","pclm"))
-  fine_method <- match.arg(rough_method, c("auto","none","sprague", "beers(ord)", 
+  rough_method <- match.arg(rough_method, c("auto","none","Carrier-Farrag", 
+                                            "KKN", "Arriaga", 
+                                            "United Nations", "Strong", 
+                                            "Zigzag", "pclm"))
+  fine_method <- match.arg(fine_method, c("auto","none","sprague", "beers(ord)", 
                                            "beers(mod)", "grabill", "pclm", 
-                                           "mono", "MAV", "uniform"))
+                                           "mono",  "uniform"))
   age_out <- match.arg(age_out, c("single","abridged","5-year"))
   
+  # Handles e.g. Total, total, t
+  Sex     <- substr(Sex,1,1) |> tolower()
+  Sex     <- match.arg(Sex, c("t","f","m"))
   
-  # simplest case, gotta cover the bases
+  # simplest case, gotta cover the bases, haha
   if (rough_method == "none" & fine_method == "none"){
     return(data_in)
   }
   
   # get variables
-  value  <- data_in[, variable, drop = TRUE]
-  age    <- data_in$Age
+  value          <- data_in[, variable, drop = TRUE]
+  age            <- data_in$Age
+  has_infants    <- age2int(age)[1] == 1 & age[1] == 0
   
   # detect incoming age categorization
-  age_in <- case_when(is_single(age) ~ "single",
-                      is_abridged(age) ~ "abridged",
-                      all((age %% 5) == 0) ~ "5-year",
-                      TRUE ~ "undetermined")
+  age_in         <- case_when(is_single(age) ~ "single",
+                              is_abridged(age) ~ "abridged",
+                              all((age %% 5) == 0) ~ "5-year",
+                              TRUE ~ "other")
+  
+  # regularize undetermined ages 
+  if (age_in == "other"){
+    value1       <- graduate_uniform(Value = value, 
+                                     Age = age)
+    age1         <- names2age(value1)
+    # If there is an infant group, we preserve it
+    if (has_infants){
+      ageN       <- calcAgeAbr(age1)
+      value      <- groupAges(Value = value1, 
+                              Age = age1, 
+                              ageN = ageN)
+      age        <- names2age(value)
+      age_in     <- "abridged"
+    } else {
+      # otherwise, group to 5-year ages
+      value      <- groupAges(Value = value1, 
+                              Age = age1, 
+                              N = 5)
+      age_in     <- "5-year"
+      age        <- names2age(value)
+    }
+    data_in <- tibble(Age = age,
+                      !!variable := value)
+  }
   
   # some helper objects for flexible outbound ages
   age1   <- min(age):max(age)
@@ -54,54 +96,151 @@ smooth_flexible <- function(data_in,
                    "single" = age1,
                    "5-year" = age1 - age1 %% 5,
                    "abridged" = calcAgeAbr(age1))
+  
+  
   # ------------------------#
   # I: Handle rough methods #
   # ------------------------#
-  if (rough_method != "none"){
-    # (1) the case of auto everything (verify arguments to pass)
-    if (rough_method == "auto"){
+  
+  # this is a fallback data5
+  data5 <- data_in |> 
+    mutate(Age = Age - Age %% 5) |> 
+    group_by(Age) |> 
+    summarize(!!variable := sum(!!sym(variable)))
+  
+  # (1) the case of auto everything (verify arguments to pass)
+  if (rough_method == "auto"){
       data1 <- graduate_auto(data_in, 
-                                age_out = "single", 
-                                variable = variable,
+                             age_out = "single", 
+                             variable = variable,
+                             u5m = u5m,
+                             Sex = Sex,
                              constrain_infants = constrain_infants)
-      data_out[, variable, drop = TRUE]
+
       if (fine_method == "auto"){
-        
+        return(data1)
       }
       
-      # or do we regroup to 5 now? In this case, if we 
-      data5 <- data_out |> 
+      # regroup to 5, overrides previous one
+      data5 <- data1 |> 
         mutate(Age = Age - Age %% 5) |> 
         group_by(Age) |> 
-        mutate(!!variable = sum(!!variable))
+        summarize(!!variable := sum(!!sym(variable)))
       
-    } else {
-      value5 <- smooth_age_5(Value = value, 
-                   Age = age, 
-                   method = rough_method)
-      age5 <- names(value5) |> as.integer()
-      
-      data5 <- tibble(Age = age5, !!variable := value5)
-    }
   }
-  # ------------------------#
-  # I: Handle fine methods  #
-  # ------------------------#
+  # if the rough method was a specific one, we overwrite the value data5
+  if (rough_method %in% c("Carrier-Farrag", "KKN", "Arriaga", 
+                          "United Nations", "Strong", "Zigzag", "pclm")){
+
+    data5 <-
+      data5 |> 
+      mutate(!!variable := smooth_age_5(Value = !!sym(variable),
+                                        Age = Age,
+                                        method = rough_method))
+  
+  }
+   
+
+  # NOTE: can't return 5-year output yet even if desired, because
+  # some graduation methods shoft between 5-year age groups, and this
+  # 'light' smoothing might be desired.
+  
+  if (fine_method == "none" & age_out == "5-year"){
+    return(data5)
+  }
+  
+  # -------------------------#
+  # II: Handle fine methods  #
+  # -------------------------#
   
   # I have an idea to keep the fine structure of our auto method while
   # constraining 5-year age groups to be whatever the above did. Odd,
-  # I know, but maximally flexible?
-  
+  # I know, but maximally flexible? Here, we are presuming that (a) either
+  # the data_in had no detectable 5-year heaping, or (b) any previously-selected
+  # rough_method will have erased detectable smoothing. Otherwise, the auto method
+  # will perturb at two levels, albeit not necessarily in the same way as if
+  # data_in had both fine and rough methods as auto.
+  if (fine_method == "none" & age_out == "single"){
+    if (age_in == "single"){
+      # this is odd indeed: under what circumstances would we want to adjust 
+      # 5-year age groups but NOT single ages? In this case, we are strict,
+      # and we preserve all proportions inside single ages
+      
+      data5 <- data5 |> 
+        rename(age5 = Age,
+               value5 = !!sym(variable) )
+      
+      data1 <-
+        data_in |> 
+        mutate(age5 = Age - Age %% 5) |> 
+        mutate(prop = !!sym(variable) / sum(!!sym(variable)), .by = age5) |> 
+        left_join(data5, by = join_by(age5)) |> 
+        mutate(!!variable := !!sym(variable) * prop) |> 
+        select(Age, !!sym(variable))
+    } else {
+      if (age_in != "single"){
+        warning("We used graduate_mono() to split to single ages.\nThis (or another fine_method) was necessary because\nyou specified single-age output, but your input\ndoes not appear to be in single ages.")
+        fine_method <- "mono"
+        # value  <- data_in |> 
+        #             pull(!!sym(variable))
+        # age    <- data_in |> 
+        #             pull(Age)
+        # value1 <- graduate_mono(Value = value,
+        #                         Age = age,
+        #                         OAG = TRUE)
+        # age1   <- names2age(value1)
+        # data1  <- tibble(Age = age1,
+        #                 !!variable := value1)
+      } 
+    }
+  } 
   if (fine_method == "auto"){
-    data_auto <- graduate_auto(data_in, 
-                               age_out = "single", 
-                               variable = variable,
-                               constraint_infant = constrain_infant)
+    # Here we presume that data5 has no detectable sawtooth pattern,
+    # meaning this was so in data_in or as the result of another 
+    # rough_method having been applied. Otherwise, this will get picked
+    # up in the auto method and taken care of with its MAV logic.
+    data1 <- graduate_auto(data5,
+                           age_out = "single",
+                           variable = variable,
+                           constrain_infants = contrain_infants,
+                           u5m = ufm,
+                           Sex = Sex)
+  }
+  if (fine_method %in% c("sprague", "beers(ord)", 
+                         "beers(mod)", "grabill", "pclm", 
+                         "mono", "uniform")){
+    value  <- data5 |> 
+                pull(!!sym(variable))
+    age    <- data5 |> 
+                pull(Age)
+    value1 <- graduate(Value = value,
+                       Age = age,
+                       method = fine_method,
+                       OAG = TRUE)
+    age1   <- names2age(value1)
+    data1  <- tibble(Age = age1,
+                    !!variable := value1)
     
     
   }
-
-  # plots <- plot_smooth_comparison(data_in, data_out)
+  # we got to single ages anyway, it's pragmatic, trust me
+  
+  #-----------------------------#
+  # III group to desired output #
+  #-----------------------------#
+  
+  # no matter what, we have a 
+  value    <- data1 |> 
+                pull(!!sym(variable))
+    age    <- data1 |> 
+                pull(Age)
+  value_out <- groupAges(Value = value,
+                         Age = age,
+                         ageN = ageN)
+  age      <- names2age(value_out)
+  data_out <- tibble(Age = age,
+                     !!variable = value_out)
+  
   
   return(data_out)
 }
