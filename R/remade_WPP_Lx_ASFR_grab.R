e2da7156d62e9410ce062d13b8ccaf856ff0c87f
timriffe <tim.riffe@gmail.com>
2025-10-14 15:55:10 +0200

    moves RtZ's caller finding code to R/package_stuff.R which is ignored

--- R/remade_WPP_Lx_ASFR_grab.R
+++ R/remade_WPP_Lx_ASFR_grab.R
@@ -441,33 +441,33 @@ download_SRB <- function(SRB = NULL,
 }
 
 # which functions should be updated (apparently one)
-library(codetools)
+# library(codetools)
+# 
+# find_callers <- function(pkg = "DemoTools", fun_name = "downloadnLx") {
+#   ns <- asNamespace(pkg)
+#   funs <- ls(ns)
+#   
+#   users <- funs[sapply(funs, function(f) {
+#     obj <- get(f, envir = ns)
+#     if (is.function(obj)) {
+#       globals <- codetools::findGlobals(obj, merge = FALSE)$functions
+#       fun_name %in% globals
+#     } else
+#       FALSE
+#   })]
+#   
+#   users
+#   
+# }
+# 
+# # Example:
+# find_callers("DemoTools", "downloadnLx")
+# find_callers("DemoTools", "downloadAsfr")
+# find_callers("DemoTools", "downloadSRB")
 
-find_callers <- function(pkg = "DemoTools", fun_name = "downloadnLx") {
-  ns <- asNamespace(pkg)
-  funs <- ls(ns)
-  
-  users <- funs[sapply(funs, function(f) {
-    obj <- get(f, envir = ns)
-    if (is.function(obj)) {
-      globals <- codetools::findGlobals(obj, merge = FALSE)$functions
-      fun_name %in% globals
-    } else
-      FALSE
-  })]
-  
-  users
-  
-}
 
-# Example:
-find_callers("DemoTools", "downloadnLx")
-find_callers("DemoTools", "downloadAsfr")
-find_callers("DemoTools", "downloadSRB")
-
-
-sexRatio1 %>%
-  as_tibble() %>%
-  subset(name %in% location, select = country_code) %>%
-  unique() %>%
-  as.numeric()
+# sexRatio1 %>%
+#   as_tibble() %>%
+#   subset(name %in% location, select = country_code) %>%
+#   unique() %>%
+#   as.numeric()
---
+++ ui_flow_control_smoothing.R
@@ -0,0 +1,48 @@
+
+
+devtools::load_all()
+library(dplyr)
+data_in <- readr::read_csv(system.file("extdata",
+                                       "abridged_hmd_spain.csv.gz",
+                                       package="ODAPbackend"))
+
+# standard checks run before doing the thing.
+initial_data_checks <- check_data(data_in)
+initial_data_checks
+
+
+head(data_in) # has .id column
+
+# Here explicitly we return the same age groups
+# used in the input data.
+
+
+
+output <- smooth_flexible(data_in,
+                          variable = "Deaths", # user specifies this
+                          age_out = "single", # always use this: not a user choice
+                          fine_method = "sprague", # user specifies from list (see below),
+                          rough_method = "none", # always use this: not a user choice
+                          constrain_infants = TRUE, # in case abridged age 0 should be maintained as-is
+                          Sex = "t", # If this is given as columnin the data, then it's detected. 
+                          # Otherwise user should specify. 
+                          by_args = NULL, # can be ignored if .id present; don't include Sex here.
+                          # in ui this is handled earlier in group definitions
+                          i18n = NULL # ui arg
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
